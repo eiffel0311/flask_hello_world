434a5ece0efaac1396f0c38e0f00162d32cd97fb
艾飞 <aif@yiche.com>
2017-04-20 09:53:19 +0800

    ./

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# coding=utf-8
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy()
+app.db = db
+
+
+def create_app():
+    from app.api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1.0')
+    return app
--- manage.py
+++ manage.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# db = SQLAlchemy()
-app = Flask(__name__)
-# db.app = app
-# db.init_app(app)
-from app.api import api as api_blueprint
-app.register_blueprint(api_blueprint, url_prefix='/api/v1.0')
 
-app.run()
+from flask_sqlalchemy import SQLAlchemy
+import sys
+from app import create_app
+
+environment =  sys.argv[1] if len(sys.argv) == 2 else "default"  # 目前只支持两个变量：　dev, pro, 分别表示开发和生产环境
+
+app = create_app()
+
+if environment in ["default", "dev"]:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1/root'
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+elif environment == "pro":
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1/root'
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+else:
+    print "wrong params"
+    sys.exit(0)
+
+db = SQLAlchemy(app)
+app.db = db
+
+
+app.run(host="127.0.0.1", port="5000")
